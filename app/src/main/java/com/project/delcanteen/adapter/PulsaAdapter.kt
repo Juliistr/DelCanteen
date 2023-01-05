c9312998edcaa125f0d6022efa77f7ca57c2d5ee
juliantisi <juliantisitorus071@gmail.com>
2023-01-05 10:55:24 +0700

    draft

---
+++ app/src/main/java/com/project/delcanteen/adapter/PulsaAdapter.kt
@@ -0,0 +1,4 @@
+package com.project.delcanteen.adapter
+
+class PulsaAdapter {
+}
---
+++ app/src/main/java/com/project/delcanteen/model/ResponUser.kt
@@ -0,0 +1,22 @@
+package com.project.delcanteen.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class ResponLogin(
+    @Expose
+    @SerializedName("name")
+    val name: String,
+    @Expose
+    @SerializedName("no_ktp")
+    val no_ktp: String,
+    @Expose
+    @SerializedName("no_hp")
+    val no_hp: String,
+    @Expose
+    @SerializedName("email")
+    val email: String,
+    @Expose
+    @SerializedName("password")
+    val password: String
+)
